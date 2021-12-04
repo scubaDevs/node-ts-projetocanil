8a37b6a2c8b81a391f85ef7ef41c8802d53fd267
Leandro Gehlen <91934483+scubaDevs@users.noreply.github.com>
2021-12-04 13:14:52 -0300

    update

--- src/Controllers/pageController.ts
+++ src/Controllers/pageController.ts
@@ -1,7 +1,7 @@
 import {Request,Response} from 'express';
 
 export const home = (req:Request, res: Response)=>{
-            res.send('home no controller.')
+            res.render('pages/page')
 };
 export const dogs = (req:Request, res: Response)=>{
 
